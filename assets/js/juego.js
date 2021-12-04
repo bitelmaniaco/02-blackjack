8d61075c8c2183676a02f0ca3dea9046eb4dfda8
bitelmaniaco <m.martindominguez@icloud.com>
2021-12-04 23:12:54 +0100

    mas cambios

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -11,6 +11,21 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0;
+let puntosCPU = 0;
+
+
+// Referencias HTML
+const btnPedir   = document.querySelector('#btnPedir');
+const btnNuevo   = document.querySelector('#btnNuevo');
+const btnDetener = document.querySelector('#btnDetener');
+
+const puntosHTML = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasCPU = document.querySelector('#maquina-cartas');
+
+
+
 // creacion de baraja
 const crearDeck = () => {
     for(let i = 2; i <= 10; i++){
@@ -26,7 +41,7 @@ const crearDeck = () => {
         }
     }
     deck = _.shuffle( deck );
-    console.log(deck);
+    //console.log(deck);
     return deck;
 }
 
@@ -52,4 +67,54 @@ const valorCarta = ( carta ) => {
             : valor * 1;
 }
 
-valorCarta('10D');
+//turno CPU
+const turnoCPU = ( puntosM ) => {
+    // divCartasCPU
+    do{
+        const carta = pedirCarta();
+        puntosCPU = puntosCPU + valorCarta( carta );
+        puntosHTML[1].innerText = puntosCPU;
+        //<img class="carta" src="assets/cartas/10D.png">
+        const imgCarta = document.createElement('img');
+        //añade la carta
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        // añade la clase
+        imgCarta.classList.add('carta');
+        divCartasCPU.append( imgCarta );
+
+    }while( puntosCPU < puntosJugador && puntosM <= 21);
+
+
+
+}
+
+
+
+
+// Eventos
+//callback
+btnPedir.addEventListener('click', () => {
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta( carta );
+    puntosHTML[0].innerText = puntosJugador;
+    //<img class="carta" src="assets/cartas/10D.png">
+    const imgCarta = document.createElement('img');
+    //añade la carta
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    // añade la clase
+    imgCarta.classList.add('carta');
+    divCartasJugador.append( imgCarta );
+
+    if( puntosJugador > 21 ) {
+        console.log('perdiste');
+        btnPedir.disabled = true;
+    } else if( puntosJugador === 21 ){
+        console.log('ganaste huevudo');
+        btnPedir.disabled = true;
+    }
+});
+
+ 
+
+
+
